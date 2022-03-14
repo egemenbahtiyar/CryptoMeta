37d66523d18863b3bffa909226b55d8c27e8137d
Egemen Bahtiyar <e.bahtiyar51@gmail.com>
2022-03-14 18:51:07 +0300

    Users can edit their profile.

--- CryptoMeta/Controllers/AccountController.cs
+++ CryptoMeta/Controllers/AccountController.cs
@@ -20,15 +20,17 @@ namespace CryptoMeta.Controllers
         public RoleManager<IdentityRole> _roleManager;
         public IEmailSender _emailSender;
         public IConfiguration _configuration;
+        public IPasswordHasher<User> _passwordHasher;
 
 
-        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager, IEmailSender emailSender, IConfiguration configuration, RoleManager<IdentityRole> roleManager )
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager, IEmailSender emailSender, IConfiguration configuration, RoleManager<IdentityRole> roleManager, ApplicationContext applicationContext, IPasswordHasher<User> passwordHasher)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _emailSender = emailSender;
             _configuration = configuration;
             _roleManager = roleManager;
+            _passwordHasher = passwordHasher;
         }
 
         public IActionResult Register()
@@ -246,6 +248,72 @@ namespace CryptoMeta.Controllers
             ModelState.AddModelError("", "Bilinmeyen hata oluştu lütfen tekrar deneyiniz.");
             return View(model);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Manage()
+        {
+            var userId = _userManager.GetUserId(HttpContext.User);
+            User user = await _userManager.FindByIdAsync(userId);
+
+            var returnedModel = new UserManageModel
+            {
+                Username = user.UserName,
+                Email = user.Email,
+                FirstName = user.FirstName,
+                LastName = user.LastName
+
+            };
+            return View(returnedModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Manage(UserManageModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var userId = _userManager.GetUserId(HttpContext.User);
+            User user = await _userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                if (!string.IsNullOrEmpty(model.Username))
+                    user.UserName = model.Username;
+                else
+                    ModelState.AddModelError("", "Username cannot be empty");
+                if (!string.IsNullOrEmpty(model.Email))
+                    user.Email = model.Email;
+                else
+                    ModelState.AddModelError("", "Email cannot be empty");
+
+                if (!string.IsNullOrEmpty(model.Password))
+                    user.PasswordHash = _passwordHasher.HashPassword(user, model.Password);
+                else
+                    ModelState.AddModelError("", "Password cannot be empty");
+                if (!string.IsNullOrEmpty(model.FirstName))
+                    user.FirstName = model.FirstName;
+                else
+                    ModelState.AddModelError("", "Firstname cannot be empty");
+                if (!string.IsNullOrEmpty(model.LastName))
+                    user.LastName = model.LastName;
+                else
+                    ModelState.AddModelError("", "LastName cannot be empty");
+
+                if (!string.IsNullOrEmpty(model.Email) && !string.IsNullOrEmpty(model.Password) && !string.IsNullOrEmpty(model.Username) && !string.IsNullOrEmpty(model.FirstName) && !string.IsNullOrEmpty(model.LastName))
+                {
+                    IdentityResult result = await _userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                        return RedirectToAction("Index","Home");
+                    else
+                        return View(model);
+                }
+            }
+            else
+                ModelState.AddModelError("", "User Not Found");
+            return View(user);
+       
+        }
+
 
     }
 }
--- CryptoMeta/Startup.cs
+++ CryptoMeta/Startup.cs
@@ -37,10 +37,10 @@ namespace CryptoMeta
             services.Configure<IdentityOptions>(options =>
             {
                 // Password settings.
-                options.Password.RequireDigit = true;
-                options.Password.RequireLowercase = true;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
                 options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequireUppercase = true;
+                options.Password.RequireUppercase = false;
                 options.Password.RequiredLength = 6;
                 options.Password.RequiredUniqueChars = 1;
 
